dab229cf95b8fdf3506c767923e90f67e2e6db55
Andrew Pecyna <pecyna.andrew@gmail.com>
2023-08-14 13:12:37 -0400

    homepage layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
@@ -7,19 +7,20 @@ import { Navbar} from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 function App() {
+  const location = useLocation();
 
   return (
   <ShoppingCartProvider>
-    <Navbar />
-    <Container className="mb-4">
+    {location.pathname === '/' ? null : <Navbar />}
+    {/* <Container className="mb-4"> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </Container>
+    {/* </Container> */}
   </ShoppingCartProvider>
-  )
+  ) 
 }
 
 export default App
